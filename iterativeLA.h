5d58efc7f7b466c8868ac5b831f889426a4a20ee
Robert Stephany <robert.stephany@utexas.edu>
2019-02-02 17:22:21 -0600

    Modified spacing (for printing)

--- iterativeLA.h
+++ iterativeLA.h
@@ -11,22 +11,22 @@ enum state {SUCCESS, WONT_STOP, BAD_DIAGONAL, BAD_DATA};
 running the jacobi function, "x" will store an approximate solution to Ax = b
 that is within the tolerance (tol) */
 state Jacobi(const Matrix & A,                   // Input: Ax = b
-	           const Vector & b,                   // Input: Ax = b
-						 Vector & x,                         // Input/Output: x stores the latest iteration
-	     			 int & maxIter,                      // Input/Output: if successful, stores the number of steps to converge
-						 const double tol);                  // Input: error tolerance
+             const Vector & b,                   // Input: Ax = b
+             Vector & x,                         // Input/Output: x stores the latest iteration
+             int & maxIter,                      // Input/Output: if successful, stores the number of steps to converge
+             const double tol);                  // Input: error tolerance
 
 state Gauss_Siedel(const Matrix & A,             // Input: Ax = b
-	           			 const Vector & b,             // Input: Ax = b
-						 		 	 Vector & x,                   // Input/Output: x stores the latest iteration
-	     			 			 int & maxIter,                // Input/Output: if successful, stores the number of steps to converge
-						 		 	 const double tol);            // Input: error tolerance
+                   const Vector & b,             // Input: Ax = b
+                   Vector & x,                   // Input/Output: x stores the latest iteration
+                   int & maxIter,                // Input/Output: if successful, stores the number of steps to converge
+                   const double tol);            // Input: error tolerance
 
 state SOR(const Matrix & A,                      // Input: Ax = b
-   			  const Vector & b,                      // Input: Ax = b
-	 		 	  Vector & x,                            // Input/Output: x stores the latest iteration
-	     		int & maxIter,                         // Input/Output: if successful, stores the number of steps to converge
-				  const double tol,                      // Input: error tolerance
-					const double w);                       // Input: Relaxation parameter
+          const Vector & b,                      // Input: Ax = b
+          Vector & x,                            // Input/Output: x stores the latest iteration
+          int & maxIter,                         // Input/Output: if successful, stores the number of steps to converge
+          const double tol,                      // Input: error tolerance
+          const double w);                       // Input: Relaxation parameter
 
 #endif
